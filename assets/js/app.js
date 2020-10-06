b8fa804dd6e6471c601607725e2d3ce8600e28f1
12wollmana <awollman2@gmail.com>
2020-10-06 13:22:16 -0500

    Import Data

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,1 +1,45 @@
-// @TODO: YOUR CODE HERE!
+/**
+ * Aaron Wollman
+ */
+
+ const elements = {
+    divScatter: d3.select("#scatter"),
+ }
+
+ const filepaths = {
+     dataCSV: "./assets/data/data.csv",
+ }
+
+
+ async function main() {
+    const data = await getData(filepaths.dataCSV);
+    console.log(data);
+ }
+
+ async function getData(filepath) {
+    let data = await d3.csv(filepath);
+
+    const numericCols = [
+        "age",
+        "ageMoe",
+        "healthcare",
+        "healthcareHigh",
+        "healthcareLow",
+        "income",
+        "incomeMoe",
+        "obesity",
+        "obesityHigh",
+        "obesityLow",
+        "smokes",
+        "smokesHigh",
+        "smokesLow"
+    ];
+
+    numericCols.forEach(column => {
+        data[column] = +data[column];
+    });
+
+    return data;
+ }
+
+ main();
